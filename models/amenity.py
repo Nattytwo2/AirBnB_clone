f08325335d3a52cf60b6c5a5272a45ea549b9413
Nattytwo2 <106903647+Nattytwo2@users.noreply.github.com>
2024-02-06 12:21:16 -0500

    updates

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines the Amenity class."""
+from models.base_model import Basemodel
+
+
+class Amenity(BaseModel):
+    """Represnt an amenity.
+    Attributes:
+    name (str): The name of the amenity.
+    """
+
+    name =""
---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python3"""Defines the city class."""
+from models.base_model import Basemodel
+
+class city(BaseModel):
+    """Represent a city.
+    Attributes:
+    state_id (str):The state id.
+    name (str): The name of the city.
+    """
+
+    state_id =""
+    name =""
+
